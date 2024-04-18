ad0c5d05e5f5e985780afb544d1cfcf72cf3c506
AzizZeynalli <a.zeynalli1@ufaz.az>
2024-04-18 20:01:53 +0400

    initial commit

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import noteReducer from './reducers/noteReducer'
+
+const store = configureStore({
+  reducer: noteReducer
+})
+
+export default store
